f76f0b7f926e1e551e0c62b230f4e9c12e8afeb6
DefDaemon <defdaemon@hotmail.com>
2021-09-23 08:49:01 +0200

    KaupenJoe tutorial #22 -> MC 1.17.1

---
+++ src/main/java/com/defdaemon/tutorialmod/core/world/ModWorldEvents.java
@@ -0,0 +1,16 @@
+package com.defdaemon.tutorialmod.core.world;
+
+import com.defdaemon.tutorialmod.TutorialMod;
+import com.defdaemon.tutorialmod.core.world.gen.ModTreeGeneration;
+import net.minecraftforge.event.world.BiomeLoadingEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = TutorialMod.MOD_ID)
+public class ModWorldEvents {
+
+    @SubscribeEvent
+    public static void biomeLoadingEvent(final BiomeLoadingEvent event) {
+        ModTreeGeneration.generateTrees(event);
+    }
+}
